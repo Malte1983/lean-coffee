4ccbb5631f54975a05a08a60b57838814eb478c6
Malte1983 <m.scherdin@gmx.de>
2021-09-30 14:44:43 +0200

    first steps for init

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Cards from './components/Cards'
+
+const exampleData = [
+  {
+    author: 'Max Muster',
+    text: 'First example-text',
+  },
+
+  {
+    author: 'Tina Müller',
+    text: 'second example-text',
+  },
+  {
+    author: 'Peter Schröder',
+    text: 'third example-text',
+  },
+  {
+    author: 'Sabrina Wilde',
+    text: 'fourth example-text',
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Cards.js
@@ -0,0 +1,10 @@
+function Cards() {
+  return (
+    <div>
+      <div>Question</div>
+      <div>Author</div>
+    </div>
+  )
+}
+
+export default Cards
