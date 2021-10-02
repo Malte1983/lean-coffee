8dfc822d67d52fca1517b7846098118eabb922fc
Malte1983 <m.scherdin@gmx.de>
2021-10-02 13:04:07 +0200

    add delete SVG

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components/macro'
+import deletelogo from '../remove.svg'
 
 function Cards({ author, text, onDeleteButtonClick, id }) {
   return (
     <Wrapper>
       <ButtonWrapper>
-        <DeleteButton onClick={() => onDeleteButtonClick(id)}>X</DeleteButton>
+        <DeleteButton onClick={() => onDeleteButtonClick(id)}>
+          <img src={deletelogo} alt="Delete"></img>
+        </DeleteButton>
       </ButtonWrapper>
       <Text>{text}</Text>
       <Author>{author}</Author>
@@ -41,9 +44,10 @@ const Author = styled.div`
 
 const DeleteButton = styled.button`
   padding: 5px;
-  background-color: black;
   color: white;
   font-weight: 800;
+  background-color: transparent;
+  border: none;
 `
 const ButtonWrapper = styled.div`
   display: flex;
